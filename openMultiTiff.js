0f743628af7e4ebc1b2ecfe1c4cbb5b80a2eade9
HongKee Moon <hkmoon@me.com>
2017-03-09 14:37:25 +0100

    Add tiff.js and examples

---
+++ openMultiTiff.js
@@ -0,0 +1,21 @@
+var fs = require('fs');
+var Tiff = require('./tiff');
+
+var filename = "images/flybrain.tiff";
+
+fs.readFile(filename, function (err, data) {
+  if (err) { throw err; }
+  tiff = new Tiff({buffer: data});
+  console.log('width:', tiff.width());
+  console.log('height:', tiff.height());
+  console.log('currentDirectory:', tiff.currentDirectory());
+  console.log('countDirectory:', tiff.countDirectory());
+
+  document.querySelector('#slider').max = tiff.countDirectory()
+
+  tiffUpdate(1);
+});
+
+window.closed = (e) => {
+  tiff.close();
+}
---
+++ openTiff.js
@@ -0,0 +1,18 @@
+var fs = require('fs');
+var Tiff = require('./tiff');
+
+var filename = "images/lena-std.tiff";
+
+fs.readFile(filename, function (err, data) {
+  if (err) { throw err; }
+  var tiff = new Tiff({buffer: data});
+  console.log('width:', tiff.width());
+  console.log('height:', tiff.height());
+  console.log('currentDirectory:', tiff.currentDirectory());
+  console.log('countDirectory:', tiff.countDirectory());
+
+  var canvas = tiff.toCanvas();
+  document.body.appendChild(canvas);
+
+  tiff.close();
+});
