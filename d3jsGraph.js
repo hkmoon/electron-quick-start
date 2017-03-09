771b18ac7a7c79596c7bf6d228aae471c24e874b
HongKee Moon <hkmoon@me.com>
2017-03-09 18:22:30 +0100

    Remove unnecessary comment

--- d3jsGraph.js
+++ d3jsGraph.js
@@ -1,7 +1,3 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
-
     var d3 = require("d3");
 
     var svg = d3.select("svg"),
